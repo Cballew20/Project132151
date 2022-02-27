566a160f2fb7e1965defb2320fc1d10d072be660
Cballew20 <89356649+Cballew20@users.noreply.github.com>
2022-02-27 17:13:59 -0600

    Add files via upload

--- js/volunteers.js
+++ js/volunteers.js
@@ -3,12 +3,17 @@ var $ = function (id) { return document.getElementById(id); };
 var volunteerArray = [];
 
 var displayVolunteers = function () {   
-    // display the volunteers in the text area
-    $("volunteerList").value = volunteerArray.join("\n");
 
-	// comment out the line above change this to a loop instead to loop through the array.
-	
-	
+// display volunteers in Text area
+var volunteerListString="";
+index=0;
+for (var i = 0; i < volunteerArray.length; i++) {
+   index= i+1;  
+   volunteerListString = volunteerListString + index + "." +volunteerArray[i]+ "\n";
+}    
+// display the volunteers in the text area
+    $("volunteerList").value = volunteerListString;
+
 };
 
 var addVolunteer = function () {
@@ -27,15 +32,16 @@ var addVolunteer = function () {
     $("first_name").focus();
 };
 
-
 var deleteVolunteer = function () {
-    // get the data from the form (hint: use the same format as from the add).
+var volunteerString = $("first_name").value + " " + $("last_name").value;
+// remove the string from the array
+for (var i = 0; i < volunteerArray.length; i++) {
+   if(volunteerArray[i].trim() == volunteerString.trim()){
+            volunteerArray.splice(i,1);
+    }
 
-    // remove the string from the array (hint, loop through the entire list, compare the string with the item in the array.
-	
-   
-	 
-    // display the volunteers and clear the add form
+}   
+ // display the volunteers and clear the add form
     displayVolunteers();
     
     // get the delete form ready for next entry
@@ -45,31 +51,16 @@ var deleteVolunteer = function () {
 };
 
 var clearList = function () {   
-    // delete the data from the arrays
     volunteerArray = [];
-    
-	//   alternative way to delete all of the data from the array
-	//    volunteerArray.length = 0;
-    
-    // remove the volunteers data from the web page
     $("volunteerList").value = "";
-    
     $("first_name").focus();
 };
 
-var sortList = function () {   
-    // sort the scores
-    volunteerArray.sort();
-    
-    // display the scores
-    displayVolunteers();    
-};
 
 //When the page is fully loaded, the buttons will be mapped to the JavaScript functions
 window.onload = function () {
     $("add_button").onclick = addVolunteer;
 	$("delete_button").onclick = deleteVolunteer;
-    $("clear_button").onclick = clearList;    
-    $("sort_button").onclick = sortList;    
+    $("clear_button").onclick = clearList;      
     $("first_name").focus();
 };
