fbab4c11d450d71002b32b58d9d8f8718263cdab
Cballew20 <89356649+Cballew20@users.noreply.github.com>
2022-03-06 19:27:31 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+window.onload = rotate;
+
+var theExchange = 0;
+var exImages = new Array("images/banner1.jpg", "images/banner2.jpg", "images/banner3.jpg");
+
+function rotate(){
+
+theExchange++;
+if( theExchange == exImages.length) {
+theExchange = 0;
+}
+document.getElementById("banner1").src = exImages [theExchange];
+setTimeout(rotate, 3000); 
+
+}
