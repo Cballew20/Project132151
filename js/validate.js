0e6fc9c80d52dc0efa3fe8cab3066d3cbf44e44b
Cballew20 <89356649+Cballew20@users.noreply.github.com>
2022-03-12 21:15:56 -0600

    Add files via upload

--- js/validate.js
+++ js/validate.js
@@ -14,7 +14,7 @@ function validate() {
 
 
     if (username == "") {
-        document.getElementById('erroruserName').innerHTML = 'Username can't be empty.';
+        document.getElementById('erroruserName').innerHTML = "Username can't be empty.";
         return false;
     } 
 
